f47b2e9514ecedf51b565e4fd48289275d8eec24
PeterVennerstrom <petervennerstrom@gmail.com>
2024-08-11 17:10:27 -0500

    Add setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="streamliner",
+    version="0.0.1",
+    url="https://github.com/mypackage.git",
+    author="PeterVennerstrom",
+    author_email="petervennerstrom@gmail.com",
+    description="abstractions for parallel ml business logic",
+    packages=find_packages(),
+    install_requires=[],
+)
